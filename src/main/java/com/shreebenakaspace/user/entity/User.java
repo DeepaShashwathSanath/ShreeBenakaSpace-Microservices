70cda91eb75c21e418f880f123f4bae48ee40562
sample <amdeepasanathshashwath@gmail.com>
2026-04-10 14:35:50 +0530

    Removed local learning files from Git tracking

--- src/main/java/com/shreebenakaspace/user/entity/User.java
+++ src/main/java/com/shreebenakaspace/user/entity/User.java
@@ -1,8 +1,14 @@
-package com.shreebenakaspace.model;
+package com.shreebenakaspace.user.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
-
-    private Long id;
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long  id;
     private String name;
     private String email;
 
